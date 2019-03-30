0be165f017325f93738572688a3a999b1ed29a92
eco <995674251@qq.com>
2019-03-30 23:20:45 +0800

    人物panel

--- js/relation.js
+++ js/relation.js
@@ -18,17 +18,63 @@ let Application = PIXI.Application,
 
 export function PersonPanel(){
 	const SPRITE = Symbol();
+	const INITCONFIG = Symbol();
 	var PersonPanel = {};
 	PersonPanel.init = function(app,config){
+	    var config = this[INITCONFIG](config); 
 		var that = this;
-		var panel = config.texture||'images/circle.png';
-		this[SPRITE] = new Sprite.fromImage(panel);
-	    this[SPRITE].width = config.width||64;
-	    this[SPRITE].height = config.height||64;
-	    this[SPRITE].x = config.x||(window.innerWidth/2 - 32);
-	    this[SPRITE].y = config.y||(window.innerHeight/2 - 32);
+		var position = {
+			x:config.x||(window.innerWidth/2 - 32),
+			y:config.y||(window.innerHeight/2 - 32)
+		}
+		var size = {
+			width:config.width||64,
+			height:config.height||64
+		}
+		var center = {
+			x:(config.width||64)/2,
+			y:(config.height||64)/2
+		}
+	    this[SPRITE] = new Container();
+	    /*外围环*/
+		var wrap = new Sprite.fromImage(config.frame||'images/circle.png');
+		this.setSize(wrap,size);
+	    /*头像*/
+	    var icon = new Sprite.fromImage(config.icon.img||'images/icon.png');
+	    var offset = {
+	    	x:config.icon.offsetX||0,
+	    	y:config.icon.offsetY||0
+	    }
+	    this.setSize(icon,size,0.6,offset);
+
+
+	    this[SPRITE].addChild(wrap,icon);
+
+
+	    this[SPRITE].style = size;
+	    this[SPRITE].position = position;
 	    app.stage.addChild(that[SPRITE]);
         return this;
+	}
+	PersonPanel[INITCONFIG] = function(config){
+		var config = config?config:{
+			width:null,
+	      	height:null,
+	      	icon:{
+	      		img:null,
+	      		offsetX:null,
+	      		offsetY:null
+	      	},
+	      	x:null,
+	      	y:null,
+	      	texture:null
+		};
+		config.icon = config.icon?config.icon:{
+      		img:null,
+      		offsetX:null,
+      		offsetY:null
+      	};
+	    return config;
 	}
 	PersonPanel.getSprite = function(){
 		return this[SPRITE];
@@ -37,6 +83,14 @@ export function PersonPanel(){
 		this.getSprite().position = {
 			x:x,
 			y:y
+		}
+	}
+	PersonPanel.setSize = function(obj,size,percent,offset){
+		obj.width = size.width * (percent||1);
+		obj.height = size.height * (percent||1);
+		obj.position = {
+			x:percent?(1-percent)*size.width/2+offset.x||0:0,
+			y:percent?(1-percent)*size.height/2+offset.y||0:0,
 		}
 	}
 	return PersonPanel;
