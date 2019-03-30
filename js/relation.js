b311ba95a2c66de1f9be488689eebb09b816fd73
eco <995674251@qq.com>
2019-03-30 21:38:44 +0800

    symbol成员变量私有化

--- js/relation.js
+++ js/relation.js
@@ -1,3 +1,12 @@
+/*!
+ * relation.js - v0.1
+ * 2019.3.30
+ * Author 桔子桑
+ *
+ * github：https://github.com/ecojust/Relation.js
+ * relation.js is licensed under the MIT License.
+ * http://www.opensource.org/licenses/mit-license
+ */
 
 let Application = PIXI.Application,
     Loader = PIXI.loader,
@@ -8,25 +17,28 @@ let Application = PIXI.Application,
 
 
 export function PersonPanel(){
-	var PersonPanel = {
-		sprite:undefined,
-	};
+	const SPRITE = Symbol();
+	var PersonPanel = {};
 	PersonPanel.init = function(app,config){
 		var that = this;
-		var panel;
-		if(!config.texture){
-			panel = "images/circle.png";
-		}else{
-			panel = texture;
-		}
-		this.sprite = new PIXI.Sprite.fromImage(panel);
-	    this.sprite.width = config.width||64;
-	    this.sprite.height = config.height||64;
-	    this.sprite.x = config.x||(window.innerWidth/2 - 32);
-	    this.sprite.y = config.y||(window.innerHeight/2 - 32);
-	    app.stage.addChild(that.sprite);
+		var panel = config.texture||'images/circle.png';
+		this[SPRITE] = new Sprite.fromImage(panel);
+	    this[SPRITE].width = config.width||64;
+	    this[SPRITE].height = config.height||64;
+	    this[SPRITE].x = config.x||(window.innerWidth/2 - 32);
+	    this[SPRITE].y = config.y||(window.innerHeight/2 - 32);
+	    app.stage.addChild(that[SPRITE]);
         return this;
 	}
+	PersonPanel.getSprite = function(){
+		return this[SPRITE];
+	}
+	PersonPanel.moveTo = function(x,y){
+		this.getSprite().position = {
+			x:x,
+			y:y
+		}
+	}
 	return PersonPanel;
-	
 }
+
